4c8c893088823ff92503b5f67e842f77a548bf46
Jacob Boodry <jake.boodry@live.com>
2018-06-20 23:09:58 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,8 @@
+var chocolateBars = ["snickers", " hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(a,e){
+  return [e, ...a]
+}
+function destructivelyAddElementToBeginningOfArray(a,e){
+  a.unshift(e)
+}
